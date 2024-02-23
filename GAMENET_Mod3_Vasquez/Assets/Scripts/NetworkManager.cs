75c0b42856e23a2a37ea2c5acac33d9f202a31f6
Ryan Lorenzo Vasquez <ryanlorenzo.vasquez@benilde.edu.ph>
2024-02-23 17:04:46 +0800

    GAMENET M3 pt1

---
+++ GAMENET_Mod3_Vasquez/Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+
+    [Header("Login UI")]
+    public GameObject LoginUIPanel;
+    public InputField PlayerNameInput;
+
+    [Header("Connecting Info Panel")]
+    public GameObject ConnectingInfoUIPanel;
+
+    [Header("Creating Room Info Panel")]
+    public GameObject CreatingRoomInfoUIPanel;
+
+    [Header("GameOptions  Panel")]
+    public GameObject GameOptionsUIPanel;
+
+    [Header("Create Room Panel")]
+    public GameObject CreateRoomUIPanel;
+    public InputField RoomNameInputField;
+
+    [Header("Inside Room Panel")]
+    public GameObject InsideRoomUIPanel;
+   
+    [Header("Join Random Room Panel")]
+    public GameObject JoinRandomRoomUIPanel;
+
+    #region Unity Methods
+    // Start is called before the first frame update
+    void Start()
+    {
+        ActivatePanel(LoginUIPanel.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    #region UI Callback Methods
+    public void OnLoginButtonClicked()
+    {
+        string playerName = PlayerNameInput.text;
+
+        if (!string.IsNullOrEmpty(playerName))
+        {
+            ActivatePanel(ConnectingInfoUIPanel.name);
+
+            if (!PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.LocalPlayer.NickName = playerName;
+                PhotonNetwork.ConnectUsingSettings();
+            }
+        }
+        else
+        {
+            Debug.Log("PlayerName is invalid!");
+        }
+    }
+
+    public void OnCancelButtonClicked()
+    {
+        ActivatePanel(GameOptionsUIPanel.name);
+    }
+    
+    public void OnCreateRoomButtonClicked()
+    {
+        string roomName = RoomNameInputField.text;
+
+        if (string.IsNullOrEmpty(roomName))
+        {
+            roomName = "Room " + Random.Range(1000, 10000);
+        }
+    }
+    #endregion
+
+    #region Photon Callbacks
+    public override void OnConnected()
+    {
+        Debug.Log("Connected to Internet");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.LocalPlayer.NickName+ " is connected to Photon");
+        ActivatePanel(GameOptionsUIPanel.name);
+    }
+
+    #endregion
+
+    #region Public Methods
+    public void ActivatePanel(string panelNameToBeActivated)
+    {
+        LoginUIPanel.SetActive(LoginUIPanel.name.Equals(panelNameToBeActivated));
+        ConnectingInfoUIPanel.SetActive(ConnectingInfoUIPanel.name.Equals(panelNameToBeActivated));
+        CreatingRoomInfoUIPanel.SetActive(CreatingRoomInfoUIPanel.name.Equals(panelNameToBeActivated));
+        CreateRoomUIPanel.SetActive(CreateRoomUIPanel.name.Equals(panelNameToBeActivated));
+        GameOptionsUIPanel.SetActive(GameOptionsUIPanel.name.Equals(panelNameToBeActivated));
+        JoinRandomRoomUIPanel.SetActive(JoinRandomRoomUIPanel.name.Equals(panelNameToBeActivated));
+    }
+    #endregion
+}
